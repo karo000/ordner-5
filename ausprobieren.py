03ddbfcdb0d04db9180d2e5f5ce4f06e0176da9a
Katharina Rohr <katharina.rohr@student.kit.edu>
2023-04-19 17:26:44 +0200

    erster commit

---
+++ ausprobieren.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+A = np.array([[0,0],
+              [2,1]])
+
+B = np.array([5,2])
+
+C = np.random.random(size=(2,3))
+c = np.array([3, 10])
+#b = np.random.random(size=(2,))
+
+#A_inv = np.transpose(A)
+
+print(3*B)
